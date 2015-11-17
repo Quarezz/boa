c54a32238fe75f5f5ab9fa6e1859842e8baa3583
Ruslan <morfed.quarezz@gmail.com>
2015-11-17 11:38:04 +0200

    Lazy initialisation on router
    Implemented VC lazy initialisation in router
    Removed rootRouter from router

--- lib/boa/module/templates/objc/Router.h
+++ lib/boa/module/templates/objc/Router.h
@@ -10,14 +10,10 @@
 #import <UIKit/UIKit.h>
 
 @class <%= @prefix %>RootRouter;
-@class <%= @prefixed_module %>ViewController;
 
 @interface <%= @prefixed_module %>Router : NSObject
 
-@property (nonatomic, strong) <%= @prefix %>RootRouter *rootRouter;
 @property (nonatomic, weak) <%= @prefixed_module %>ViewController *viewController;
-
--(instancetype) init;
 
 - (void)presentFromViewController:(UIViewController *)viewController;
 
--- lib/boa/module/templates/objc/Router.m
+++ lib/boa/module/templates/objc/Router.m
@@ -14,36 +14,26 @@
 #import "<%= @prefixed_module %>Interactor.h"
 #import "<%= @prefixed_module %>Presenter.h"
 
-@interface <%= @prefixed_module %>Router ()
-
-@end
-
 @implementation <%= @prefixed_module %>Router
-
--(instancetype) init
-{
-    if (self = [super init])
-    {
-      // instantiate viewController
-      // self.viewController = ...;
-
-      // view <-> presenter
-      <%= @prefixed_module %>Presenter *presenter = [<%= @prefixed_module %>Presenter new];
-
-      presenter.view = self.viewController;
-      self.viewController.presenter = presenter;
-
-      // interactor <-> presenter
-      <%= @prefixed_module %>Interactor *interactor = [<%= @prefixed_module %>Interactor new];
-      presenter.interactor = interactor;
-      interactor.presenter = presenter;
-    }
-    return self;
-}
 
 - (void)presentFromViewController:(UIViewController *)viewController
 {
-    // present controller
+    // instantiate viewController
+    // <%= @prefixed_module %>ViewController *viewController = ...;
+
+    // view <-> presenter
+    <%= @prefixed_module %>Presenter *presenter = [<%= @prefixed_module %>Presenter new];
+
+    presenter.router = self;
+    presenter.view = self.viewController;
+    viewController.presenter = presenter;
+
+    // interactor <-> presenter
+    <%= @prefixed_module %>Interactor *interactor = [<%= @prefixed_module %>Interactor new];
+    presenter.interactor = interactor;
+    interactor.presenter = presenter;
+
+    // present viewController
 }
 
 @end
