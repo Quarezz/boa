b7cfbfe28801c5ee2295af1c1079742df3d6096c
Ruslan Nikolaev <r.nikolaev@smartweb.com.ua>
2016-07-19 11:24:44 +0300

    removed unneeded class

--- lib/boa/module/templates/objc/DependenciesHead.m
+++
@@ -1,4 +0,0 @@
-#import "<%= @prefixed_module %>Router.h"
-#import "<%= @prefixed_module %>Presenter.h"
-#import "<%= @prefixed_module %>DataManager.h"
-#import "<%= @prefixed_module %>Interactor.h"
